aeca94aec4f439575c3f93d3be91b7ddf8ffdce6
K CHaitu <kchaithanya.2010@gmail.com>
2021-11-25 22:40:53 +0530

    build

---
+++ jenkinsbuild.groovy
@@ -0,0 +1,29 @@
+//declarative pipeline
+pipeline{
+    agent any
+    parameters {
+        string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'type branch name here')
+    }
+    stages{
+        stage("clone code"){
+            steps{
+                println "cloning the code from git hub"
+            }
+        }
+        stage("buid"){
+            steps{
+                println "build code"
+                sh "mvn clean package"
+                sh "ls -l"
+            }
+        }
+        stage("save artifacts in s3"){
+            steps{
+                println " upload artifacts to s3"
+                sh "echo $BUILD_NUM"
+                sh "aws s3 cp target/hello-${BUILD_NUM}.war s3://chaituart/"
+            }
+        }
+
+    }
+}
